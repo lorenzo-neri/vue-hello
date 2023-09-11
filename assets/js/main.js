79891d310edb8cde916ef9f687e69ff432cc0526
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-11 14:37:12 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/* 
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa inserendo il percoso in una propietá dell'oggetto restituito dalla funzione data.
+*/
+
+const { createApp } = Vue
+        
+        createApp({
+            data() {
+                return {
+                    title: 'Hello Vue! Nice to meet you!'
+                }
+            }
+        }).mount('#app')
